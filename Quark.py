d30c710a8547abd98cdb3060a490e980b92613e1
Shimgo2008 <{Shimgo2008@gmail.com}>
2025-07-31 16:13:55 +0900

    feat: Structure quark properties into Flavor enum

--- Quark.py
+++ Quark.py
@@ -1,22 +1,41 @@
 from enum import Enum
-from typing import Literal, Tuple
 from dataclasses import dataclass
 
 from SciFloat import SciFloat
+from Units import eV, eUnit
+
+
+@dataclass(frozen=True)
+class QuarkProperty:
+    charge: SciFloat
+    mass: SciFloat
+    gen: int
 
 
 class Flavor(Enum):
-    UP = "up"
-    DOWN = "down"
-    CHARM = "charm"
-    STRANGE = "strange"
-    TOP = "top"
-    BOTTOM = "bottom"
+    UP = QuarkProperty(charge=SciFloat(2, 0) / SciFloat(3, 0), mass=SciFloat(2.2, 6), gen=1)
+    DOWN = QuarkProperty(charge=SciFloat(-1, 0) / SciFloat(3, 0), mass=SciFloat(4.7, 6), gen=1)
+    CHARM = QuarkProperty(charge=SciFloat(2, 0) / SciFloat(3, 0), mass=SciFloat(1.27, 9), gen=2)
+    STRANGE = QuarkProperty(charge=SciFloat(-1, 0) / SciFloat(3, 0), mass=SciFloat(96, 6), gen=2)
+    TOP = QuarkProperty(charge=SciFloat(2, 0) / SciFloat(3, 0), mass=SciFloat(173.1, 9), gen=3)
+    BOTTOM = QuarkProperty(charge=SciFloat(-1, 0) / SciFloat(3, 0), mass=SciFloat(4.18, 9), gen=3)
+
+    @property
+    def charge(self) -> SciFloat:
+        return self.value.charge
+
+    @property
+    def mass(self) -> SciFloat:
+        return self.value.mass
+
+    @property
+    def gen(self) -> int:
+        return self.value.gen
 
 
 class Spin(Enum):
-    UP = SciFloat(5, -1)
-    DOWN = SciFloat(-5, -1)
+    UP = SciFloat(0.5, 0)
+    DOWN = SciFloat(-0.5, 0)
 
 
 class Color(Enum):
@@ -25,11 +44,39 @@ class Color(Enum):
     BLUE = "blue"
 
 
-@dataclass
+@dataclass(frozen=True)
 class Quark:
+    """
+    素粒子であるクォークを表現するクラス。
+
+    Attributes
+    ----------
+    flavor : Flavor
+        クォークのフレーバー (UP, DOWNなど)。
+    color : Color
+        クォークの色荷 (RED, GREEN, BLUE)。
+    spin : Spin
+        クォークのスピン (UP, DOWN)。
+    """
+
     flavor: Flavor
     color: Color
     spin: Spin
-    charge: SciFloat
-    position: Tuple[SciFloat, SciFloat, SciFloat]
-    gen: Literal[1, 2, 3]
+
+    @property
+    def charge(self) -> eUnit:
+        """クォークの電荷を返す。"""
+        return eUnit(self.flavor.charge)
+
+    @property
+    def mass(self) -> eV:
+        """クォークの質量を返す。"""
+        return eV(self.flavor.mass)
+
+    @property
+    def gen(self) -> int:
+        """クォークの世代を返す。"""
+        return int(self.flavor.gen)
+
+    def __str__(self) -> str:
+        return f"Quark(flavor={self.flavor.name}, color={self.color}, spin={self.spin.name})"
